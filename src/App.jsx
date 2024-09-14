14175bef9adc9dc0abf343ec5d3eacc26950b97b
“JavadRafieii” <j.rafiei999@gmail.com>
2024-09-14 13:57:31 +0330

    App is ready to start (installed Tailwind CSS, Swiper, Parallax, Google Fonts and React Scroll).

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from "./components/container/Container";
+
+function App() {
+  return (
+    <main className="w-full max-w-[1920px] mx-auto overflow-hidden">
+      <Container>
+        <h1 className="text-3xl font-bold underline">
+          Hello Captain Hook!
+        </h1>
+      </Container>
+    </main>
+  );
+}
+
+export default App;
