90b5deb8b2f675d10ab7755a8ab98ad47ebfddab
“JavadRafieii” <j.rafiei999@gmail.com>
2024-09-16 17:18:31 +0330

    Completed small button component.

--- src/components/play/Play.jsx
+++ src/components/play/Play.jsx
@@ -1,5 +1,7 @@
 import Container from "../container/Container";
+import SmallButton from "../smallButton/SmallButton";
 import Cover from "/cover.png";
+import Button from "/button-1.png";
 
 function Play() {
     return (
@@ -8,7 +10,7 @@ function Play() {
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 xl:gap-10 2xl:gap-20">
                     <div className="flex items-center">
                         <figure className="w-full">
-                            <img src={Cover} alt="cover" loading="lazy" className="w-full"/>
+                            <img src={Cover} alt="cover" loading="lazy" className="w-full" />
                         </figure>
                     </div>
                     <div>
@@ -17,6 +19,9 @@ function Play() {
                             <p className="open-sans-Medium text-smoky-gray text-lg">
                                 Set sail on an unforgettable fishing journey with Captain Hook, your gateway to a thrilling play-to-earn adventure on the Binance Smart Chain. Cast your line into a vibrant ocean teeming with opportunities, reel in diverse fish, and transform your catches into real rewards. As you dive deeper, enhance your ship and gear to boost your earning potential and efficiency. Whether you’re new to the play-to-earn scene or a seasoned player, Captain Hook offers a captivating and rewarding experience. Join the adventure and chart your course to becoming the ultimate sea captain!
                             </p>
+                        </div>
+                        <div className="flex justify-center -translate-y-[25px] lg:-translate-y-[55px]">
+                            <SmallButton src={Button} text={"Play now"} link={"#"} />
                         </div>
                     </div>
                 </div>
