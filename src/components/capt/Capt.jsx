b09d8337e9975fc0f32c9f1f345f649ddf07e204
“JavadRafieii” <j.rafiei999@gmail.com>
2024-09-21 16:15:33 +0330

    Compeleted treasure and labals.

--- src/components/capt/Capt.jsx
+++ src/components/capt/Capt.jsx
@@ -5,6 +5,13 @@ import BgMobile2 from "/presale-bg-4-mobile.jpg";
 import Container from "../container/Container";
 import BigButton from "../bigButton/BigButton";
 import Treasure from "/treasure.png";
+import Staking from "/Staking.png";
+import RewardPool from "/RewardPool.png";
+import Liquidity from "/Liquidity.png";
+import Marketing from "/Marketing.png";
+import DevTeam from "/Dev-Team.png";
+import PreSale from "/Pre-Sale.png";
+import Treasury from "/Treasury.png";
 import Bg3 from "/presale-bg-5.jpg";
 import BgMobile3 from "/presale-bg-5-mobile.jpg";
 import Boat from "/boat.png";
@@ -34,7 +41,14 @@ function Capt() {
             </div>
             <div className="relative">
                 <figure className="absolute z-20 w-[800px] h-[760px] left-[calc(50%-400px)] top-[calc(50%-380px)] scale-[.5] sm:scale-[.7] md:scale-[.8] lg:scale-[.6] xl:scale-[1] xxl:scale-[1.2]">
-                    <img src={Treasure} alt="treasure" loading="lazy" className="w-full h-full"/>
+                    <img src={Treasure} alt="treasure" loading="lazy" className="w-full h-full" />
+                    <img src={Staking} alt="Staking" loading="lazy" className="labal-1 absolute left-[43px] top-[180px]" />
+                    <img src={RewardPool} alt="Reward Pool" loading="lazy" className="labal-7 absolute left-[95px] top-[428px]" />
+                    <img src={Liquidity} alt="Liquidity" loading="lazy" className="labal-3 absolute left-[225px] top-[120px]" />
+                    <img src={Marketing} alt="Marketing" loading="lazy" className="labal-4 absolute left-[245px] top-[275px]" />
+                    <img src={DevTeam} alt="Dev & Team" loading="lazy" className="labal-5 absolute left-[380px] top-[100px]" />
+                    <img src={PreSale} alt="Pre Sale" loading="lazy" className="labal-6 absolute right-[35px] top-[200px]" />
+                    <img src={Treasury} alt="Treasury" loading="lazy" className="labal-2 absolute right-[110px] top-[350px]" />
                 </figure>
                 <img src={Bg2} alt="background CAPT TOKEN" loading="lazy" className="hidden lg:block w-full" />
                 <img src={BgMobile2} alt="background CAPT TOKEN" loading="lazy" className="lg:hidden w-full" />
