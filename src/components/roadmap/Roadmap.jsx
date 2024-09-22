660bed916c8659ab5901498e93d70505dc9b9cf9
“JavadRafieii” <j.rafiei999@gmail.com>
2024-09-22 16:01:38 +0330

    Added AOS animation.

--- src/components/roadmap/Roadmap.jsx
+++ src/components/roadmap/Roadmap.jsx
@@ -1,15 +1,24 @@
+import { useEffect } from "react";
 import Container from "../container/Container";
 import DATA from "../../../data/data";
+import AOS from 'aos'
 import "./style.css";
+import 'aos/dist/aos.css';
 
 function Roadmap() {
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
+
     return ( 
         <div className="Roadmap bg-purple-gradient py-20 xl:py-40">
             <Container>
                 <h3 className="relative z-20 ribeye-regular text-white text-4xl text-center mb-20 xl:mb-40">Roadmap</h3>
                 <div className="relative z-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-10 sm:gap-5 xl:gap-0">
                     {DATA.ROADMAP.map(item => (
-                        <div key={item.id}>
+                        <div key={item.id} data-aos="fade-up" data-aos-delay={item.id * 100} data-aos-duration="1000" data-aos-offset="300">
                             <h4 className="ribeye-regular text-white text-2xl lg:text-xl xl:text-2xl text-center">{item.title}</h4>
                             <p className="text-white text-xl text-center my-3">{item.subtitle1}</p>
                             <h4 className="ribeye-regular text-white text-2xl lg:text-xl xl:text-[1.1rem] text-center">{item.subtitle2}</h4>
--- src/components/stage/Stage.jsx
+++ src/components/stage/Stage.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
+
 import DATA from "../../../data/data";
+import AOS from 'aos';
 import "./style.css";
+import 'aos/dist/aos.css';
 
 function Stage() {
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
     return (
         DATA.STAG.map(item => (
             <div key={item.id} className="xl:hover:scale-[1.1] xl:transition-[1s]">
-                <div className="flex flex-col items-center gap-2" data-stage={item.title}>
+                <div className="flex flex-col items-center gap-2" data-stage={item.title} data-aos="fade-up" data-aos-delay={item.id * 100} data-aos-duration="1000" data-aos-offset="300">
                     <h4 className="ribeye-regular text-smoky-gray text-xl xl:text-2xl text-center">{item.title}</h4>
                     <div className="flex flex-col items-center open-sans-Medium text-smoky-gray text-lg">
                         <span>Price</span>
